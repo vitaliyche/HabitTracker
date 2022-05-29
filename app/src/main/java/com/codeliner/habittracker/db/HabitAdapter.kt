8a7e49e4437252ab6ebb1ccec360cd52df3d7e7c
vitaliyche <vit.chernyavsky@gmail.com>
2022-05-29 13:42:02 +0300

    Привычки обнуляются на следующий день

--- app/src/main/java/com/codeliner/habittracker/db/HabitAdapter.kt
+++ app/src/main/java/com/codeliner/habittracker/db/HabitAdapter.kt
@@ -17,18 +17,18 @@ import java.util.*
 
 class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, HabitAdapter.ItemHolder>(ItemComparator()) { //добавили listener, который нужно передать в setData
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder { //создаем viewHolder // когда мы запустим адаптер на нашем активити, то сюда передаст ViewGroup
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
         return ItemHolder.create(parent) // функция для каждой заметки из базы данных будет создавать собственный itemholder
-    } //который в себе будет создавать разметку для каждого элемента
+    } //создаем viewHolder // когда мы запустим адаптер на нашем активити, то сюда передаст ViewGroup, который в себе будет создавать разметку для каждого элемента
 
-    override fun onBindViewHolder(holder: ItemHolder, position: Int) { // заполнение разметки сразу после создания //интерфейс один и тот же добавится на все слушатели из списка
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
         holder.setData(getItem(position), listener) //28 listener присваивается каждому элементу
-    }
+    } // заполнение разметки сразу после создания //интерфейс один и тот же добавится на все слушатели из списка
 
-    class ItemHolder(view: View): RecyclerView.ViewHolder(view) {  //в ItemHolder хранится ссылка на разметку и элементы
+    class ItemHolder(view: View): RecyclerView.ViewHolder(view) {
         private val binding = HabitNameItemBinding.bind(view)
 
-        fun setData(habitNameItem: HabitNameItem, listener: Listener) = with(binding) { // с помощью setdata можем заполнять разметку
+        fun setData(habitNameItem: HabitNameItem, listener: Listener) = with(binding) {
             tvHabitName.text = habitNameItem.name //получаем доступ к элементам
             tvTime.text = habitNameItem.time
             pBar.max = habitNameItem.allItemCounter//48 максимальный прогресс прогрессбара - это всего задач
@@ -37,14 +37,12 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
             pBar.progressTintList = colorState //48 tintList принимает colorState
             counterCard.backgroundTintList = colorState //48 цвет счетчика меняется в зависимости все или нет задачи выполнили
 
-            //2203 переопределить checkedItemsCounter (0 или 1) и allItemCounter (запланировано дней в неделю)
-
             val counterText = "${habitNameItem.checkedHabitCounter}/${habitNameItem.planDaysPerWeek}"//48 текст нужно составлять из разных частей, поэтому создадим отдельную переменную
             tvCounter.text = counterText
             itemView.setOnClickListener { //itemView - при нажатии будем открывать наш список
                 listener.onClickItem(habitNameItem, NAME) //220315 будет открываться список задач
             }
-            ibDelete.setOnClickListener{ //28 если нажмем на кнопку Удалить, то запускается слушатель,
+            ibDelete.setOnClickListener{ //28 если нажмем на кнопку Удалить, запускается слушатель,
                 listener.deleteItem(habitNameItem.id!!) //28 который возвращает от каждого элемента идентификатор
             } //28 и по этому идентификатору мы можем удалить из БД данный элемент
             chBoxHabit.isChecked = habitNameItem.habitChecked
@@ -53,32 +51,27 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
                 if(chBoxHabit.isChecked) {
                     listener.onClickItem (habitNameItem.copy(
                         habitChecked = chBoxHabit.isChecked,
-                        checkedHabitCounter = habitNameItem.checkedHabitCounter+1,
-                        checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR-1)),
+                        checkedHabitCounter = habitNameItem.checkedHabitCounter + 1,
+                        checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)), // DAY_OF_YEAR)-1 использовать для теста
                         CHECK_BOX) //0322 записываем true или false
                 } else {
                     listener.onClickItem (habitNameItem.copy(
                         habitChecked = chBoxHabit.isChecked,
-                        checkedHabitCounter = habitNameItem.checkedHabitCounter-1),
+                        checkedHabitCounter = habitNameItem.checkedHabitCounter - 1),
                         CHECK_BOX) //0322 записываем true или false
-                }//нужно проверить, что непрочекано, только тогда добавляем+1
+                } //нужно проверить, что непрочекано, только тогда добавляем+1
             } //0322 для сохранения состояния нужно сделать апдейт в БД Dao
             ibEdit.setOnClickListener{ //29 при нажатии на кнопку edit запускается listener // и editItem интерфейс, который нужно добавить во фрагмент
                 listener.editItem(habitNameItem) //29 передается весь элемент
             }
-
-//            //обнулить галочки, если день отличается от дня, в котором эти галочки были установлены
-//            val today = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
-//            if (checkedHa)
-
-        }
+        } // с помощью setdata можем заполнять разметку
 
         private fun getProgressColorState(item: HabitNameItem, context: Context): Int { //48 цвет для прогрессбара
-            return if (item.checkedItemsCounter == item.allItemCounter) { //48 если отмечены все элементы в списке
-                ContextCompat.getColor(context, R.color.green_main) //48 цвет зеленый
-            } else { //48 если не все элементы отмечены
+            return if (item.checkedItemsCounter == item.allItemCounter) {
+                ContextCompat.getColor(context, R.color.green_main) //48 если отмечены все элементы в списке, цвет зеленый
+            } else {
                 ContextCompat.getColor(context, R.color.red_main) //48 цвет прогрессбара красный
-            }
+            } //48 если не все элементы отмечены
         } //48 цвет нужно применить в setData
 
         private fun setPaintFlagAndColor(binding: HabitNameItemBinding) { //38 чтобы textView был перечеркнут
@@ -97,16 +90,16 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
             } //38 запуск функции из setData
         }
 
-        companion object{ // когда запускаем ItemHolder, // Create выдает уже инициализированный класс holder, // который хранит в себе ссылку на загруженную в память разметку
+        companion object{
             fun create(parent: ViewGroup): ItemHolder{ //инициализируем ItemHolder
                 return ItemHolder( //возвращаем ItemHolder с загруженной разметкой
                     LayoutInflater.from(parent.context). //надуваем разметку
                     inflate(R.layout.habit_name_item, parent, false))
             }
-        }
-    }
+        } // когда запускаем ItemHolder, Create выдает уже инициализированный класс holder, который хранит в себе ссылку на загруженную в память разметку
+    } //в ItemHolder хранится ссылка на разметку и элементы
 
-    class ItemComparator : DiffUtil.ItemCallback<HabitNameItem>() { //diffUtil сравнивает элементы из старого и нового списков
+    class ItemComparator : DiffUtil.ItemCallback<HabitNameItem>() {
 
         override fun areItemsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean { //сравнивает похожи ли элементы
             return oldItem.id == newItem.id //сравниваем идентификаторы, чтобы узнать это один и тот же item // и нужно ли обновлять список
@@ -115,7 +108,7 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
         override fun areContentsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean { //сравнивает весь контент внутри элемента
             return oldItem == newItem
         }
-    }
+    } //diffUtil сравнивает элементы из старого и нового списков
 
     interface Listener {
         fun deleteItem(id: Int) //удаление записи из БД
@@ -123,8 +116,8 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
         fun onClickItem(habitNameItem: HabitNameItem, state: Int)
     }
 
-    companion object { //40 чтобы передавать только одну функцию в нашем интерфейсе и с помощью условия определять что нужно сделать
+    companion object {
         const val NAME = 0
         const val CHECK_BOX = 1
-    }
+    } //40 чтобы передавать только одну функцию в нашем интерфейсе и с помощью условия определять что нужно сделать
 }
