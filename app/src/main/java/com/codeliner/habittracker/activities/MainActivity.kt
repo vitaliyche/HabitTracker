367d36b0060c2f6b7253d8cd7e59cdb969037828
vitaliyche <vit.chernyavsky@gmail.com>
2022-06-27 20:19:37 +0300

    Изменен интерфейс

--- app/src/main/java/com/codeliner/habittracker/activities/MainActivity.kt
+++ app/src/main/java/com/codeliner/habittracker/activities/MainActivity.kt
@@ -21,27 +21,38 @@ import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
+
     lateinit var binding: ActivityMainBinding
     private lateinit var defPref: SharedPreferences
+    private lateinit var pref: SharedPreferences
+
     private var currentMenuItemId = R.id.habits_list //54 это id нижнего меню, по умолчанию habits_list
     private var currentTheme = ""
     private var iAd: InterstitialAd? = null //57 переменная, куда сохраняем рекламу, когда она уже загружена
     private var adShowCounter = 0 //57 счетчик нажатий для показа Interstitial рекламы
     private var adShowCounterMax = 2 //57 количество нажатий для показа Interstitial рекламы
-    private lateinit var pref: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         defPref = PreferenceManager.getDefaultSharedPreferences(this)//55 инициализация настроек
         setTheme(getSelectedTheme()) //55 чтобы тема обновилась, нужно запускать функцию перед super.onCreate, до выбора экрана
         super.onCreate(savedInstanceState)
+
         pref = getSharedPreferences(BillingManager.MAIN_PREF, MODE_PRIVATE) //61 константа MODE_PRIVATE уже есть в активити, поэтому пишем напрямую, без контекста
+
         binding = ActivityMainBinding.inflate(layoutInflater) //инициализация binding, подключение разметки к активити
         setContentView(binding.root)
+
         currentTheme = defPref.getString("theme_key", "blue").toString() //56 как только создается активити, мы уже знаем какая тема выбрана, проверяем на отличие в onResume
+
         FragmentManager.setFragment(HabitNamesFragment.newInstance(), this) //24 фрагмент, который отображается при запуске приложения
         setBottomNavListener() //запуск функции
+
         if (!pref.getBoolean(BillingManager.REMOVE_ADS_KEY, false)) loadInterAd() //61 если записано false (нет покупки), то реклама запускается
-    }
+
+    } // onCreate
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
@@ -54,7 +65,6 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
                     } //onFinish - сработает только когда объявление закрыли или не загрузилось
                 })
             }
-
             R.id.menu_new_habit -> {
                 FragmentManager.currentFrag?.onClickNew()
             }
@@ -63,20 +73,17 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
         return super.onOptionsItemSelected(item)
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
-//    private fun habitCheckedOff(startDate: Int) {
-//        var checkedOffHabits: Boolean = true
-//        if(Calendar.getInstance().get(Calendar.DAY_OF_YEAR) - startDate !=0) {
-//            checkedOffHabits = false
-//        } else checkedOffHabits = true
-//    }
 
     private fun loadInterAd() {
+
         val request = AdRequest.Builder().build() //57 создали запрос на получение рекламы
+
         InterstitialAd.load(this, getString(R.string.inter_ad_id), request,
             object : InterstitialAdLoadCallback(){
                 override fun onAdLoaded(ad: InterstitialAd) { //57 когда успешно загрузилось объявление
@@ -88,9 +95,12 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
         })
     }
 
+
     private fun showInterAd(adListener: AdListener) {
+
         if (iAd != null  && adShowCounter > adShowCounterMax && !pref.getBoolean(BillingManager.REMOVE_ADS_KEY, false)) { //57 если объявление загрузилось или количество нажатий больше заданного
             iAd?.fullScreenContentCallback = object : FullScreenContentCallback() { //fullScreenContentCallback - слушатель на каком этапе объявление
+
                 override fun onAdDismissedFullScreenContent() { //57 когда user закрывает объявление //57 его перебрасывает в Настройки SettingsActivity
                     iAd = null //57 обнуляем объявление, потому что уже просмотрено
                     loadInterAd() //57 загружаем новое объявление
@@ -114,9 +124,12 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
             adShowCounter++ //57 считаем количество нажатий
             adListener.onFinish() //57 перенаправляем пользователя на нужный ему экран
         }
-    }
+
+    } // showInterAd
+
 
     private fun setBottomNavListener() {
+
         binding.bNav.setOnItemSelectedListener {
 
             when(it.itemId) {
@@ -129,7 +142,6 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
                         }
                     })
                 }
-
                 R.id.habits_list -> { //если выбран habitNamesFragment и мы нажмем на кнопку New,
                     currentMenuItemId = R.id.habits_list //54 сохранено, что было выбрано, когда зашли в Настройки
                     FragmentManager.setFragment(HabitNamesFragment.newInstance(), this) //то запустится onClickNew во фрагменте HabitNamesFragment
@@ -140,28 +152,38 @@ class MainActivity : AppCompatActivity(), NewHabitDialog.Listener {
             true
 
         } // setOnItemSelectedListener
-    } // fun setBottomNavListener
+
+    } // setBottomNavListener
+
 
     override fun onResume() { //54 когда возвращаемся с активити Настроек
         super.onResume()
+
         binding.bNav.selectedItemId = currentMenuItemId //54 какая кнопка должна быть нажата, когда возвращаемся
         if (defPref.getString("theme_key", "blue") != currentTheme) //56 если тема изменилась
             recreate() // //56 recreate пересоздает активити для моментального обновления темы
+
     } //54 записываем currentMenuItemId в setBottomNavListener
 
+
     private fun getSelectedTheme(): Int{ //55 функция выбора темы
+
         return if (defPref.getString("theme_key", "blue") == "blue") {
             R.style.Theme_HabitTrackerBlue
         } else {
             R.style.Theme_HabitTrackerRed
         }
+
     } //55 эту же функцию повторить в SettingsActivity, чтобы тема обновлялась и на экране настроек
+
 
     override fun onClick(name: String, days: String) { //23 имплементировали функцию, запускается после нажатия кнопки в диалоге
         Log.d("mylog", "Name: $name, Days: $days") //23 в этой функции онклик нужно будет сохранять в БД эту привычку,
     } //23 чтобы позже появлялся recycler view, появлялись все названия привычек в виде item-элементов
 
+
     interface AdListener {
         fun onFinish() //57 объявление было просмотрено
     } //57 интерфейс передаем в функцию showInterAd
+
 }
--- app/src/main/java/com/codeliner/habittracker/db/HabitAdapter.kt
+++ app/src/main/java/com/codeliner/habittracker/db/HabitAdapter.kt
@@ -14,36 +14,50 @@ import com.codeliner.habittracker.R
 import com.codeliner.habittracker.databinding.HabitNameItemBinding
 import com.codeliner.habittracker.entities.HabitCheckedItem
 import com.codeliner.habittracker.entities.HabitNameItem
+import com.codeliner.habittracker.utils.TimeManager
 import java.util.*
 
-class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, HabitAdapter.ItemHolder>(ItemComparator()) { //добавили listener, который нужно передать в setData
+
+class HabitAdapter(
+    private val listener: Listener, // нужно передать в setData
+    //TODO: передать checkedListener для сохранения данных в таблицу HabitCheckedItem
+    //private val checkedlistener: CheckedListener
+    ): ListAdapter<HabitNameItem, HabitAdapter.ItemHolder>(ItemComparator()) {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        return ItemHolder.create(parent) // функция для каждой заметки из базы данных будет создавать собственный itemholder
-    } //создаем viewHolder // когда мы запустим адаптер на нашем активити, то сюда передаст ViewGroup, который в себе будет создавать разметку для каждого элемента
+        return ItemHolder.create(parent)
+    }
+
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-        holder.setData(getItem(position), listener) //28 listener присваивается каждому элементу
-    } // заполнение разметки сразу после создания //интерфейс один и тот же добавится на все слушатели из списка
+        holder.setData(getItem(position), listener) // listener присваивается каждому элементу
+    }
+
 
     class ItemHolder(view: View): RecyclerView.ViewHolder(view) {
+
         private val binding = HabitNameItemBinding.bind(view)
+
 
         fun setData(habitNameItem: HabitNameItem, listener: Listener) = with(binding) {
 
             tvHabitName.text = habitNameItem.name //получаем доступ к элементам
             tvTime.text = habitNameItem.time
+
             pBar.max = habitNameItem.allItemCounter//48 максимальный прогресс прогрессбара - это всего задач
             pBar.progress = habitNameItem.checkedItemsCounter //48 на сколько мы продвинулись, сколько раз выполнили
             val colorState = ColorStateList.valueOf(getProgressColorState(habitNameItem, binding.root.context))
             pBar.progressTintList = colorState //48 tintList принимает colorState
             counterCard.backgroundTintList = colorState //48 цвет счетчика меняется в зависимости все или нет задачи выполнили
+
             val counterText = "${habitNameItem.checkedHabitCounter}/${habitNameItem.planDaysPerWeek}"//48 текст нужно составлять из разных частей, поэтому создадим отдельную переменную
             tvCounter.text = counterText
 
-            itemView.setOnClickListener { //itemView - при нажатии будем открывать наш список
-                listener.onClickItem(habitNameItem, NAME) //220315 будет открываться список задач
-            }
+
+            itemView.setOnClickListener {
+                listener.onClickItem(habitNameItem, NAME) //будет открываться список задач
+            } //itemView - при нажатии открыть список
 
             ibDelete.setOnClickListener{ //28 если нажмем на кнопку Удалить, запускается слушатель,
                 listener.deleteItem(habitNameItem.id!!) //28 который возвращает от каждого элемента идентификатор
@@ -52,42 +66,73 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
             chBoxHabit.isChecked = habitNameItem.habitChecked
             setPaintFlagAndColor(binding) //39 запускаем функцию перечеркивания текста один раз, когда обновляется адаптер
 
-            chBoxHabit.setOnClickListener { //0322 слушаем нажатие чекбокса выполнения привычки
+
+            chBoxHabit.setOnClickListener {
 
                 if(chBoxHabit.isChecked) {
 
                     listener.onClickItem (habitNameItem.copy(
                         habitChecked = chBoxHabit.isChecked,
+                        time = TimeManager.getCurrentTime(),
                         checkedHabitCounter = habitNameItem.checkedHabitCounter + 1,
                         checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)), // DAY_OF_YEAR)-1 использовать для теста
                         CHECK_BOX) //0322 записываем true или false
+
+                     //TODO: сохранить данные в таблицу HabitCheckedItem
+/*                    listener.onClickItem (habitCheckedItem.copy(
+                        habitChecked = chBoxHabit.isChecked,
+                        checkedHabitCounter = habitNameItem.checkedHabitCounter + 1,
+                        checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)), // DAY_OF_YEAR)-1 использовать для теста
+                        CHECK_BOX) //0322 записываем true или false*/
 
                 } else {
                     listener.onClickItem (habitNameItem.copy(
                         habitChecked = chBoxHabit.isChecked,
                         checkedHabitCounter = habitNameItem.checkedHabitCounter - 1),
                         CHECK_BOX) //0322 записываем true или false
-                } //нужно проверить, что непрочекано, только тогда добавляем+1
-            } //0322 для сохранения состояния нужно сделать апдейт в БД Dao
 
-            ibEdit.setOnClickListener{ //29 при нажатии на кнопку edit запускается listener // и editItem интерфейс, который нужно добавить во фрагмент
+                    // TODO: сохранить данные в таблицу HabitCheckedItem
+/*                    listener.onClickItem (habitCheckedItem.copy(
+                        habitChecked = chBoxHabit.isChecked,
+                        checkedHabitCounter = habitNameItem.checkedHabitCounter - 1),
+                        CHECK_BOX) //0322 записываем true или false*/
+
+                } // else - если непрочекано
+
+            } // chBoxHabit.setOnClickListener - слушать нажатие чекбокса выполнения привычки
+
+
+            ibEdit.setOnClickListener {
                 listener.editItem(habitNameItem) //29 передается весь элемент
-            }
+            } // при нажатии на кнопку edit запускается listener и editItem интерфейс, который нужно добавить во фрагмент
 
         } // SetData - с помощью setdata можем заполнять разметку
 
 
-        // при нажатии на checked нужно добавить данные во вторую БД или удалить, если галочка убрана
-        /*fun setHabitCheckedItem(habitCheckedItem: HabitCheckedItem, listener: Listener) = with(binding) {
+        // TODO: при нажатии на checked нужно добавить данные во вторую БД или удалить, если галочка убрана
+        /*fun setHabitCheckedItem(habitCheckedItem: HabitCheckedItem, checkedlistener: CheckedListener)
+            = with(binding) {
 
-            chBoxHabit.setOnClickListener { //0322 слушаем нажатие чекбокса выполнения привычки
+            chBoxHabit.setOnClickListener {
 
-                if(chBoxHabit.isChecked) {
+                if (chBoxHabit.isChecked) {
 
-                    listener.onClickItem (habitCheckedItem.copy(
-                        habitId = tvHabitName.toString(),
-                        checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)),
-                        )
+                    checkedlistener.onClickItem(
+                        habitCheckedItem.copy(
+                            habitId = tvHabitName.toString(),
+                            checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
+                        ),
+                        CHECK_BOX
+                    )
+                } else {
+                    //TODO: удалить строку из таблицы HabitCheckedItem
+                    checkedlistener.onClickItem(
+                        habitCheckedItem.copy(
+                            habitId = tvHabitName.toString(),
+                            checkedHabitDay = Calendar.getInstance().get(Calendar.DAY_OF_YEAR-1)
+                        ),
+                        CHECK_BOX
+                    )
                 }
             }
         }*/
@@ -116,25 +161,28 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
             } //38 запуск функции из setData
         }
 
-        companion object{
-            fun create(parent: ViewGroup): ItemHolder{ //инициализируем ItemHolder
-                return ItemHolder( //возвращаем ItemHolder с загруженной разметкой
-                    LayoutInflater.from(parent.context). //надуваем разметку
-                    inflate(R.layout.habit_name_item, parent, false))
+        companion object {
+            fun create(parent: ViewGroup): ItemHolder{
+                return ItemHolder(
+                    LayoutInflater.from(parent.context).inflate(R.layout.habit_name_item,
+                    parent, false))
             }
         } // когда запускаем ItemHolder, Create выдает уже инициализированный класс holder, который хранит в себе ссылку на загруженную в память разметку
-    } //в ItemHolder хранится ссылка на разметку и элементы
+
+    } // ItemHolder - хранится ссылка на разметку и элементы
+
 
     class ItemComparator : DiffUtil.ItemCallback<HabitNameItem>() {
 
-        override fun areItemsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean { //сравнивает похожи ли элементы
-            return oldItem.id == newItem.id //сравниваем идентификаторы, чтобы узнать это один и тот же item // и нужно ли обновлять список
+        override fun areItemsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean {
+            return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean { //сравнивает весь контент внутри элемента
+        override fun areContentsTheSame(oldItem: HabitNameItem, newItem: HabitNameItem): Boolean {
             return oldItem == newItem
         }
-    } //diffUtil сравнивает элементы из старого и нового списков
+    } // diffUtil - сравнивает элементы из старого и нового списков
+
 
     interface Listener {
         fun deleteItem(id: Int) //удаление записи из БД
@@ -142,8 +190,17 @@ class HabitAdapter(private val listener: Listener): ListAdapter<HabitNameItem, H
         fun onClickItem(habitNameItem: HabitNameItem, state: Int)
     }
 
+    //TODO: реализовать интерфейс для HabitCheckedItem, чтобы записывать туда данные
+    interface CheckedListener {
+        fun deleteItem(id: Int) //удаление записи из БД
+        fun editItem(habitCheckedItem: HabitCheckedItem) //29 редактирование записи из БД. Передаем полностью название, потому что будем изменять
+        fun onClickItem(habitCheckedItem: HabitCheckedItem, state: Int)
+    }
+
+
     companion object {
         const val NAME = 0
         const val CHECK_BOX = 1
     } //40 чтобы передавать только одну функцию в нашем интерфейсе и с помощью условия определять что нужно сделать
+
 }
